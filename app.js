840528f5b4b641b374b2ca7f38c58cfec5cf83c2
Hamzagecer <etemmmete14@gmail.com>
2021-03-08 21:16:36 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+// 1 ) fruits arrayini araya cizgi (-) koyarak ekrana yazdiriniz
+// 2 ) animals arrayindeki son elemani silebilirmisiniz
+// 3 ) fruits arayine (kivi) ekleyebilirmisiniz
+// 4 ) animals arrayinin ilk ogesini silebilirmisini
+// 5 ) fruits arrayinden (lemon, apple,mango) elemanlarini silip Kiwi ekleyebilirmisiniz
+// 6 ) fruits ve animals araylerini birlestirebilirmisiniz
+// 7 ) iki arayin son elemanlarinin index numaralarini ekrana yazdirabilirmisiniz
+
+const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+const animals=["Cat", "Dog", "Elephon", "Rabid"]
+
