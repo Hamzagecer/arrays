3ace0b62d84681e3be2d2870dec567190e1b87cf
Hamzagecer <etemmmete14@gmail.com>
2021-03-10 01:59:31 +0100

    first commit

--- app.js
+++ app.js
@@ -9,3 +9,33 @@
 const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
 const animals=["Cat", "Dog", "Elephon", "Rabid"]
 
+
+
+//  1-  console.log(fruits.join("-"));
+
+//  2-  const pops = animals.pop();
+//      console.log( animals);
+//      const splices = animals.splice(3,4);
+//      console.log(animals);
+
+//  3-  const kivi = fruits.push('Kivi');
+//      console.log(fruits);
+
+//  4-  const shifts = animals.shift();
+//      console.log( animals);
+
+
+//  5-  const hg = fruits.splice(2,3,);
+//      const gh = fruits.push('kivi');
+//      console.log(fruits);
+
+//  6-  console.log(fruits.concat(animals));
+
+//  7-  console.log(fruits.indexOf("Mango"));
+//      console.log(fruits.length-1);
+//      console.log(animals.indexOf("Rabid"));
+//      console.log(animals.length-1);
+
+// not: hepsini yorum satırı seklınde teslım edıyorum cunku ayrı ayrı bakıldıgında sonucu tam verıyor ..Denerken farkettiğim 2 örnegi fazladan eklemek istedim 2. ve 7. cevapta mesela 2. cevapta animals.splice(3,4); denilidği zamanda aynı sonuca ulasabıldıgımı farkettım yada 7. cevapta dizinin uzunlugundan 1 cıkarınca yıne istenen sonuca ulasabıldıgını farkettım 1 i neden cıkardım cunku ıcınde toplam bulunan elaman sayısını verıyordu son sayının index numarası değildi bu yüzden 1 cıkarttım .. Umarım anlasılır . sorulan sorularla cevapların sırası aynıdır..
+
+
